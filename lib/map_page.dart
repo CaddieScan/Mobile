de2daa31a14d65e5cc14f6cc1696dfffad3ddf54
TheStarBars <aoges@orange.fr>
2026-05-06 22:40:20 +0200

    ADD: display map

--- lib/map_page.dart
+++ lib/map_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:caddiescan/models/zone.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'components/container_map.dart';
 
@@ -11,25 +12,25 @@ class MapPage extends StatelessWidget {
   const MapPage({super.key});
 
   Future<List<Zone>> fetchZones() async {
+    final prefs = await SharedPreferences.getInstance();
+    final shopIdStr = prefs.getString("current_shop_id_scan");
     String baseUrl = dotenv.env['API_BASE_URL'] ?? '';
-    final response = await http.get(Uri.parse('$baseUrl/stores/1/map'));
-    print("Body: ${response.body}"); // <--- C'est ici que tu verras la vérité
+    print("URL: $baseUrl");
+    final response = await http.get(Uri.parse('$baseUrl/api/stores/$shopIdStr/map/mobile'));
+    print("Body: ${response.body}");
     if (response.statusCode == 200) {
       final dynamic decoded = jsonDecode(response.body);
 
-      // On vérifie si la réponse est bien une Map et si la clé existe
       if (decoded is Map<String, dynamic> && decoded['zones'] != null) {
         final List<dynamic> zoneList = decoded['zones'];
         return zoneList.map((data) => Zone.fromJson(data)).toList();
       } else if (decoded is List) {
-        // Si le back renvoie directement une liste []
         return decoded.map((data) => Zone.fromJson(data)).toList();
       }else {
         throw Exception("Erreur Serveur : ${response.statusCode}");
       }
     }
 
-    // Si on arrive ici, c'est qu'il y a un problème
     throw Exception("Format de données invalide ou erreur serveur");
   }
 
